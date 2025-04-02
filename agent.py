7b0c36869a54fce92f6dd8a461e572b2a3e60213
Zaid <karaymehzaid@gmail.com>
2025-04-03 00:18:21 +0300

    init

---
+++ agent.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import logging
+from dotenv import load_dotenv
+
+from livekit import rtc
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    WorkerOptions,
+    cli,
+    llm,
+)
+from livekit.agents.multimodal import MultimodalAgent
+from livekit.plugins import openai
+from prompt import AGENT_PROMPT
+
+load_dotenv(dotenv_path=".env.local")
+logger = logging.getLogger("my-worker")
+logger.setLevel(logging.INFO)
+
+
+async def entrypoint(ctx: JobContext):
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    participant = await ctx.wait_for_participant()
+
+    run_multimodal_agent(ctx, participant)
+
+    logger.info("agent started")
+
+
+def run_multimodal_agent(ctx: JobContext, participant: rtc.RemoteParticipant):
+    logger.info("starting multimodal agent")
+
+    model = openai.realtime.RealtimeModel(
+        voice="coral",
+        instructions=AGENT_PROMPT,
+        modalities=["audio", "text"],
+    )
+
+    # create a chat context with chat history, these will be synchronized with the server
+    # upon session establishment
+    chat_ctx = llm.ChatContext()
+    chat_ctx.append(
+        text="مرحبًا بك في مستشفى النور. كيف يمكنني مساعدتك اليوم؟ هل ترغب في حجز موعد أو فتح ملف مريض جديد؟",
+        role="assistant",
+    )
+
+    agent = MultimodalAgent(
+        model=model,
+        chat_ctx=chat_ctx,
+    )
+    agent.start(ctx.room, participant)
+
+    # to enable the agent to speak first
+    agent.generate_reply()
+
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+        )
+    )
